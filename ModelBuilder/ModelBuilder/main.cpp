d0e8e85fb38d281f12357b10a1e51162627f0953
Denis Burov <denis.burov@spl.co>
2017-01-19 11:56:09 +0300

    1.0 finished

--- ModelBuilder/ModelBuilder/main.cpp
+++ ModelBuilder/ModelBuilder/main.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-enum ModelTypeId {
+enum class TypeId {
 	tBoolean,
 	tInteger,
 	tFloat,
@@ -16,34 +17,32 @@ enum ModelTypeId {
 	tArrayCustomModel
 };
 
-struct ModelType {
-	ModelTypeId id;
-	string customTypeName;
+struct ObjectType {
+	TypeId id;
+	string name;
 };
 
 struct Field {
 	string name;
-	ModelType type;
+	ObjectType type;
 };
 
 struct Model {
-	string name;
+	ObjectType type;
 	vector<Field*> fields;
 };
 
-
-
-ModelTypeId getType(string str) {
-	if (str == "boolean") return tBoolean;
-	if (str == "integer") return tInteger;
-	if (str == "number") return tFloat;
-	if (str == "string") return tString;
+TypeId getType(string str) {
+	if (str == "boolean") return TypeId::tBoolean;
+	if (str == "integer") return TypeId::tInteger;
+	if (str == "number") return TypeId::tFloat;
+	if (str == "string") return TypeId::tString;
 	if (str.substr(0, 6) == "Array[") {
-		if (str.substr(6, 3) == "int") return tArrayInteger;
-		if (str.substr(6, 3) == "str") return tArrayString;
-		return tArrayCustomModel;
+		if (str.substr(6, 3) == "int") return TypeId::tArrayInteger;
+		if (str.substr(6, 3) == "str") return TypeId::tArrayString;
+		return TypeId::tArrayCustomModel;
 	}
-	return tCustomModel;
+	return TypeId::tCustomModel;
 }
 
 string removeArrayText(string str) {
@@ -59,52 +58,171 @@ string removeArrayText(string str) {
 	return str.substr(start, end - start);
 }
 
-string getCustomTypeName(string str) {
-	ModelTypeId type = getType(str);
-	switch (type) {
-	case tBoolean:
-	case tInteger:
-	case tFloat:
-	case tString:
-		return str;
-	case tArrayInteger:
-	case tArrayString:
-		return removeArrayText(str);
-	case tArrayCustomModel:
-	case tCustomModel:
-		str = removeArrayText(str);
-		size_t toPos;
-		toPos = str.find("TO");
+void setObjectType(ObjectType& type, string text) {
+	type.id = getType(text);
+	size_t toPos;
+	switch (type.id) {
+	case TypeId::tBoolean:
+		type.name = "boolean";
+		break;
+	case TypeId::tInteger:
+		type.name = "int";
+		break;
+	case TypeId::tFloat:
+		type.name = "float";
+		break;
+	case TypeId::tString:
+		type.name = "String";
+		break;
+	case TypeId::tArrayInteger:
+		type.name = "int[]";
+	case TypeId::tArrayString:
+		type.name = "String[]";
+	case TypeId::tCustomModel:
+		toPos = text.find("TO");
 		if (toPos != string::npos) {
-			str.erase(toPos, 2);
+			text.erase(toPos, 2);
 		}
-		return str += "Model";
-	default:
-		return str;
+		type.name = text + "Model";
+		break;
+	case TypeId::tArrayCustomModel:
+		text = removeArrayText(text);
+		toPos = text.find("TO");
+		if (toPos != string::npos) {
+			text.erase(toPos, 2);
+		}
+		type.name = "List<" + text + "Model>";
+		break;
 	}
 }
 
-string getTypeName(ModelTypeId type) {
-	switch (type) {
-	case tBoolean:
-		return "boolean";
-	case tInteger:
-		return "int";
-	case tFloat:
-		return "float";
-	case tString:
-		return "String";
-	case tCustomModel:
-		return "tCustomModel";
-	case tArrayInteger:
-		return "tArrayInteger";
-	case tArrayString:
-		return "tArrayString";
-	case tArrayCustomModel:
-		return "tArrayCustomModel";
-	default:
-		return "PARSE ERROR";
+string getJsonConstructorTextline(Field field) {
+	switch (field.type.id) {
+	case TypeId::tBoolean:
+		return "this." + field.name + " = getBooleanField(in, \"" + field.name + "\", false);";
+	case TypeId::tInteger:
+		return "this." + field.name + " = getIntField(in, \"" + field.name + "\", 0);";
+	case TypeId::tFloat:
+		return "this." + field.name + " = getFloatField(in, \"" + field.name + "\", 0.0f);";
+	case TypeId::tString:
+		return "this." + field.name + " = getStringField(in, \"" + field.name + "\", \"\");";
+	case TypeId::tCustomModel:
+		return "this." + field.name + " = getModel(in, \"" + field.name + "\", " + field.type.name + ".class);";
+	case TypeId::tArrayInteger:
+		return "this." + field.name + " = getIntArray(in, \"" + field.name + "\");";
+	case TypeId::tArrayString:
+		return "this." + field.name + " = getStringArray(in, \"" + field.name + "\");";
+	case TypeId::tArrayCustomModel:
+		return "this." + field.name + " = getModelList(in, \"" + field.name + "\", " + field.type.name + ".class);";
 	}
+}
+
+string getParcelConstructorTextline(Field field) {
+	switch (field.type.id) {
+	case TypeId::tBoolean:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.readByte() != 0;";
+	case TypeId::tInteger:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.readInt();";
+	case TypeId::tFloat:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.readFloat();";
+	case TypeId::tString:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.readString();";
+	case TypeId::tCustomModel:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.readParcelable(" + field.type.name + ".class.getClassLoader());";
+	case TypeId::tArrayInteger:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.createIntArray();";
+	case TypeId::tArrayString:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.createStringArray();";
+	case TypeId::tArrayCustomModel:
+		return "if (ParcelUtils.readBooleanFromParcel(in)) this." + field.name + " = in.createTypedArrayList(" + field.type.name + ".CREATOR);";
+	}
+}
+
+string getWriteToParcelTextline(Field field) {
+	switch (field.type.id) {
+	case TypeId::tBoolean:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeByte(" + field.name + " ? (byte)1 : (byte)0);";
+	case TypeId::tInteger:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeInt(" + field.name + ");";
+	case TypeId::tFloat:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeFloat(" + field.name + ");";
+	case TypeId::tString:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeString(" + field.name + ");";
+	case TypeId::tCustomModel:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeParcelable(" + field.name + ", 0);";
+	case TypeId::tArrayInteger:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeIntArray(" + field.name + ");";
+	case TypeId::tArrayString:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeStringArray(" + field.name + ");";
+	case TypeId::tArrayCustomModel:
+		return "if (ParcelUtils.writeBooleanToParcel(dest, " + field.name + ")) dest.writeTypedList(" + field.name + ");";
+	}
+}
+
+string getGetterTextline(Field field) {
+	return "    public " + field.type.name + (field.type.id != TypeId::tBoolean ? " get" : " is") + static_cast<char>(toupper(field.name[0])) + field.name.substr(1, field.name.length() - 1) + "() {\n"
+		 + "        return " + field.name + ";\n"
+		 + "    }\n";
+}
+
+void createModelFile(Model& model) {
+	ofstream modelFile;
+	string filename = model.type.name + ".java";
+	modelFile.open(filename, ios_base::trunc);
+	modelFile << "package ru.pochtabank.fakturadbo.api.model.faktura;" << endl;
+	modelFile << endl;
+	modelFile << "import android.os.Parcel;" << endl;
+	modelFile << "import com.google.gson.JsonObject;" << endl;
+	modelFile << "import java.util.List;" << endl;
+	modelFile << "import ru.pochtabank.dbo.base.model.BaseModel;" << endl;
+	modelFile << "import ru.pochtabank.utils.data.ParcelUtils;" << endl;
+	modelFile << endl;
+	modelFile << "public class " << model.type.name << " extends BaseModel {" << endl;
+	modelFile << endl;
+	modelFile << "    public static final Creator<" << model.type.name << "> CREATOR = new Creator<" << model.type.name << ">() {" << endl;
+	modelFile << "        @Override" << endl;
+	modelFile << "        public " << model.type.name << " createFromParcel(Parcel source) {" << endl;
+	modelFile << "            return new " << model.type.name << "(source);" << endl;
+	modelFile << "        }" << endl;
+	modelFile << endl;
+	modelFile << "        @Override" << endl;
+	modelFile << "        public " << model.type.name << "[] newArray(int size) {" << endl;
+	modelFile << "            return new " << model.type.name << "[size];" << endl;
+	modelFile << "        }" << endl;
+	modelFile << "    };" << endl;
+	modelFile << endl;
+	for (const auto& field : model.fields) {
+		modelFile << "    private " << field->type.name << " " << field->name << ";" << endl;
+	}
+	modelFile << endl;
+	modelFile << "    public " + model.type.name + "(JsonObject in) {" << endl;
+	for (const auto& field : model.fields) {
+		modelFile << "        " << getJsonConstructorTextline(*field) << endl;
+	}
+	modelFile << "    }" << endl;
+	modelFile << endl;
+	modelFile << "    public " + model.type.name + "(Parcel in) {" << endl;
+	for (const auto& field : model.fields) {
+		modelFile << "        " << getParcelConstructorTextline(*field) << endl;
+	}
+	modelFile << "    }" << endl;
+	modelFile << endl;
+	modelFile << "    @Override" << endl;
+	modelFile << "    public void writeToParcel(Parcel dest, int flags) {" << endl;
+	for (const auto& field : model.fields) {
+		modelFile << "        " << getWriteToParcelTextline(*field) << endl;
+	}
+	modelFile << "    }" << endl;
+	modelFile << endl;
+	modelFile << "    @Override" << endl;
+	modelFile << "    public int describeContents() {" << endl;
+	modelFile << "        return 0;" << endl;
+	modelFile << "    }" << endl;
+	modelFile << endl; 
+	for (const auto& field : model.fields) {
+		modelFile << getGetterTextline(*field) << endl;
+	}
+	modelFile.close();
 }
 
 void main() {
@@ -119,7 +237,7 @@ void main() {
 			if (textLine[0] >= 'A' && textLine[0] <= 'Z') {
 				string name = textLine.substr(0, textLine.find_first_of(" "));
 				newModel = new Model();
-				newModel->name = getCustomTypeName(name);
+				setObjectType(newModel->type, name);
 				models.push_back(newModel);
 			}
 			// new field in current model
@@ -130,8 +248,7 @@ void main() {
 				string typeText = textLine.substr(openPos, closePos - openPos);
 				Field* newField = new Field();
 				newField->name = nameText;
-				newField->type.id = getType(typeText);
-				newField->type.customTypeName = getCustomTypeName(typeText);
+				setObjectType(newField->type, typeText);
 				newModel->fields.push_back(newField);
 			}
 		}
@@ -139,20 +256,6 @@ void main() {
 	}
 
 	for (const auto& model : models) {
-		cout << model->name << endl;
-		for (const auto& field : model->fields) {
-			switch (field->type.id) {
-			case tArrayInteger:
-			case tArrayString:
-			case tArrayCustomModel:
-				cout << " - " << "List<" << field->type.customTypeName << "> " << field->name << endl;
-				break;
-			default:
-				cout << " - " << field->type.customTypeName << " " << field->name << endl;
-			}
-		}
+		createModelFile(*model);
 	}
-
-	char a;
-	std::cin >> a;
 }
